ad6450b1bbd92db5e1637f48b604a0a627ac8979
sumancrest0001 <suman.crest0001@gmail.com>
2020-03-23 19:25:43 +0545

    initial setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Welcome to my bookstore.</p>
+    </div>
+  );
+}
+
+export default App;
