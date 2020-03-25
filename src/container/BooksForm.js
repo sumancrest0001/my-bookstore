7ac2444f0a6004d96ed03a85896eb9b25799d7ca
sumancrest0001 <suman.crest0001@gmail.com>
2020-03-25 21:51:31 +0545

    added state to the BookForm components and made it class

--- src/container/BooksForm.js
+++ src/container/BooksForm.js
@@ -1,29 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './BooksForm.module.css';
 
-const bookForm = () => {
-  const catagories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-  const renderOptions = catagories.map(option => (
-    <option
-      key={option}
-      value={option}
-    >
-      {option}
-    </option>
-  ));
+class BookForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      book: {
+        title: '',
+        category: '',
+      },
+    };
+  }
 
-  return (
-    <div className={classes.Form}>
-      <form>
-        <h3>Add new to Bookstore</h3>
-        <input placeholder="Enter Book Title" type="text" required />
-        <select>
-          {renderOptions}
-        </select>
-        <button name="add" type="submit">ADD</button>
-      </form>
-    </div>
-  );
-};
 
-export default bookForm;
+  render() {
+    const catagories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+    return (
+      <div className={classes.Form}>
+        <form>
+          <h3>Add new to Bookstore</h3>
+          <input placeholder="Enter Book Title" type="text" required />
+          <select>
+            {
+              catagories.map(option => (
+                <option
+                  key={option}
+                  value={option}
+                >
+                  {option}
+                </option>
+              ))
+            }
+          </select>
+          <button name="add" type="submit">ADD</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default BookForm;
