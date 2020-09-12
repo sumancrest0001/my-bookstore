68cf245ccec0aa4a54e7d088c5cce1f9ae27e100
sumancrest0001 <suman.crest0001@gmail.com>
2020-09-12 20:38:00 +0545

    implement payment successfully

---
+++ client/src/components/CartItem/CartItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './CartItem.scss';
+
+const cartItem = ({ item }) => (
+  <div className="cart-item">
+    <img src={item.url} className="cart-item__image" alt={item.ittle} />
+    <div className="cart-item__details">
+      <span className="cart-item__title">{item.title}</span>
+      <span className="cart-item__price">
+        $
+        {item.quantity}
+        X $
+        {item.price}
+      </span>
+    </div>
+  </div>
+);
+
+export default cartItem;
---
+++ client/src/components/StripeButton/StripeButton.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import axios from 'axios';
+import StripeCheckout from 'react-stripe-checkout';
+
+const stripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price;
+  const publishableKey = 'pk_test_51HP3SAAHgbVhvnWLRQeyHPSn0F9xzwKrAcEGutmMTu7YthA5EQq2sw8a86LYnW7gFhl7FYacVTV8pD1thWHpxZ3d009pgqHTKu';
+
+  const onToken = token => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then(response => {
+        alert('Payment successful');
+      })
+      .catch(error => {
+        console.log('Payment error:', JSON.parse(error));
+        alert('There is an issue with your payment. Please use the correct credentials');
+      })
+  }
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='bookmandala pvt. ltd.'
+      billingAddress
+      shippingAddress
+      description={`Your total is $${price / 100}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default stripeCheckoutButton;
